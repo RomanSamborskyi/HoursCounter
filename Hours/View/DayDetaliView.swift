24ae4af9b46825cb556648d6b5cc63610f7f6c03
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2023-11-23 17:47:19 +0100

    added new constraints to launch screen

--- Hours/View/DayDetaliView.swift
+++ Hours/View/DayDetaliView.swift
@@ -29,62 +29,67 @@ struct DayDetaliView: View {
                 .padding(.bottom)
                 .font(.system(size: 20, weight: .bold, design: .rounded))
                 .multilineTextAlignment(.center)
-                HStack(alignment: .bottom) {
-                    Text("Start work at:")
-                        Spacer()
-                    VStack {
-                        Text("hour")
-                            .foregroundColor(.gray.opacity(0.7))
-                            .font(.caption2)
-                        Text("\(hours.startHours)")
+            HStack(alignment: .bottom) {
+                Text("Start work at:")
+                Spacer()
+                VStack {
+                    Text("hour")
+                        .foregroundColor(.gray.opacity(0.7))
+                        .font(.caption2)
+                    Text("\(hours.startHours)")
                     
-                    }.frame(width: 45)
-                    VStack {
-                        Text("minutes")
-                            .foregroundColor(.gray.opacity(0.7))
-                            .font(.caption2)
-                        Text("\(hours.startMinutes)")
-                    }.frame(width: 45)
-                }
-                HStack {
-                 Text("End work at:")
-                       Spacer()
-                    VStack {
-                        Text("\(hours.endHours)")
-                    }.frame(width: 45)
-                    VStack {
-                        Text("\(hours.endMinutes)")
-                    }.frame(width: 45)
-                }
-                HStack {
-                 Text("Pause time:")
-                       Spacer()
-                    Text("\(hours.pauseTime)")
-                        .frame(width: 45)
-                }
-                HStack {
-                    Text("Hours today:")
-                           Spacer()
-                    Text("\(hours.hours).\(hours.minutes)")
-                        .frame(width: 45)
-                }
+                }.frame(width: 45)
+                VStack {
+                    Text("minutes")
+                        .foregroundColor(.gray.opacity(0.7))
+                        .font(.caption2)
+                    Text("\(hours.startMinutes)")
+                }.frame(width: 45)
+            }
+            HStack {
+                Text("End work at:")
+                Spacer()
+                VStack {
+                    Text("\(hours.endHours)")
+                }.frame(width: 45)
+                VStack {
+                    Text("\(hours.endMinutes)")
+                }.frame(width: 45)
+            }
+            HStack {
+                Text("Pause time:")
+                Spacer()
+                Text("\(hours.pauseTime)")
+                    .frame(width: 45)
+            }
+            HStack {
+                Text("Hours today:")
+                Spacer()
+                Text("\(hours.hours).\(hours.minutes)")
+                    .frame(width: 45)
+            }
             if hours.hours >= 9 {
-                Text("Nice work, keep it up")
-                    .padding()
+                HStack {
+                    Text("Nice work, keep it up")
+                    Image(systemName: "checkmark.seal.fill")
+                } .padding()
                     .font(.system(size: 30, weight: .bold, design: .rounded))
                     .multilineTextAlignment(.center)
                     .background(Color.gray.opacity(0.5))
                     .clipShape(RoundedRectangle(cornerRadius: 15))
-                Image(systemName: "")
             } else {
-                Text("You can do better")
-                    .padding()
+                HStack {
+                    Text("You can do better")
+                    Spacer()
+                    Image(systemName: "figure.strengthtraining.traditional")
+                }.padding()
+                    .frame(maxWidth: .infinity)
                     .font(.system(size: 30, weight: .bold, design: .rounded))
                     .multilineTextAlignment(.center)
                     .background(Color.gray.opacity(0.5))
                     .clipShape(RoundedRectangle(cornerRadius: 15))
             }
-          Spacer()
+            Spacer()
         }.padding(25)
     }
 }
